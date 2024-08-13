d4e46eff45d3f889fd63041ee2bb717a3ac79dbf
unknownguy321 <159646845+unknownguy321@users.noreply.github.com>
2024-08-13 14:46:29 +0530

    Create EmployeeService.java

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/Employee Management System - Customizing Data Source Configuration/EmployeeService.java
@@ -0,0 +1,13 @@
+@Service
+public class EmployeeService {
+
+    @Autowired
+    @Qualifier("primaryJdbcTemplate")
+    private JdbcTemplate primaryJdbcTemplate;
+
+    @Autowired
+    @Qualifier("secondaryJdbcTemplate")
+    private JdbcTemplate secondaryJdbcTemplate;
+
+    // Use primaryJdbcTemplate or secondaryJdbcTemplate for specific operations
+}
