93cfd2d8a9e96cb260c924e00809d789dd0fc371
unknownguy321 <159646845+unknownguy321@users.noreply.github.com>
2024-08-13 14:38:21 +0530

    Create EmployeeProjection.java

---
+++ Week3/Spring Data JPA with Spring Boot, Hibernate/Employee Management System - Creating Projections/EmployeeProjection.java
@@ -0,0 +1,15 @@
+package com.example.employeemanagementsystem.projection;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public interface EmployeeProjection {
+
+    Long getId();
+    String getName();
+    String getEmail();
+    String getDepartmentName();
+
+    // Use @Value to concatenate name and email
+    @Value("#{target.name + ' (' + target.email + ')'}")
+    String getNameWithEmail();
+}
